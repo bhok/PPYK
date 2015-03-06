b0c1408b8b14787507a165a8cc1667ff092adf65
bhok <bhokor@gmail.com>
2015-03-06 01:08:18 +0100

    Update fairrpc.py

--- contrib/fairrpc/fairrpc.py
+++ contrib/fairrpc/fairrpc.py
@@ -10,9 +10,9 @@ rpcpass = ""
 
 
 if rpcpass == "":
-	access = ServiceProxy("http://127.0.0.1:46393")
+	access = ServiceProxy("http://127.0.0.1:6647")
 else:
-	access = ServiceProxy("http://"+rpcuser+":"+rpcpass+"@127.0.0.1:46393")
+	access = ServiceProxy("http://"+rpcuser+":"+rpcpass+"@127.0.0.1:6647")
 cmd = sys.argv[1].lower()
 
 if cmd == "backupwallet":
